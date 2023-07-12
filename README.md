f110f40bd2274bcde514f6fcdaf6c90d383a4984
eyedeekay <idk@mulder>
2023-07-12 12:57:34 -0400

    page generation update for: 2023-07-12 12:57:34.898093033 -0400 EDT m=+0.060969817

---
+++ README.md
@@ -0,0 +1,5 @@
+I2P Bookmarks from Not_Bob's blog
+=================================
+
+This is a WIP which creates a browser extension from blog entries
+created by addressbook curator and I2P community member not_bob.
--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-TASSEL_NAME="tassel"
-TASSEL_DESC="Template project for adding bookmarks to a web browser with an extension"
+TASSEL_NAME="botbob.i2p blog bookmarks"
+TASSEL_DESC="Bookmarks scraped from not_bob's review blog for URL completion, filtering, and search in I2P browsers"
 TASSEL_VERSION=0.0.1
-TASSEL_HOME="https://github.com/eyedeekay/Tassel"
-TASSEL_HOST="eyedeekay.github.io"
+TASSEL_HOME="https://github.com/eyedeekay/notbob_bookmarks"
+TASSEL_HOST="eyedeekay.github.io/notbob_bookmarks"
